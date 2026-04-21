991c909210310afb2e82e2634aad5a76011195ed
Ricardo <richavesca@est.utn.ac.cr>
2026-04-21 16:59:40 -0600

    Se creo el component Texto
    Este componente se creo con los parametros de alineacion, color y el parametro texto

---
+++ src/Componentes/Texto.jsx
@@ -0,0 +1,18 @@
+export default function Texto({ 
+    texto, 
+    alineado="center", 
+    color_text="black" }) {
+
+    const alineacion = {
+        "left": "text-start",
+        "center": "text-center",
+        "right": "text-end"
+    }[alineado];
+
+    return (
+
+        <p className={`${alineacion}`} style={{color: color_text}}>
+            {texto}
+        </p>
+    )
+}
