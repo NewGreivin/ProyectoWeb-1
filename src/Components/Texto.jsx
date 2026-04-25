82e444d1eb940d18e93d0bd3756751af53604074
Ricardo <richavesca@est.utn.ac.cr>
2026-04-25 14:27:10 -0600

    Se actualizo el componente texto
    Se actulizo el componente texto para poder tener diferentes tipos de tamaño segun las necesidades ahora los diferentes tamaños van un incremental de 1 en 1 hasta el 6

--- src/Components/Texto.jsx
+++ src/Components/Texto.jsx
@@ -1,7 +1,10 @@
 export default function Texto({ 
     texto, 
     alineado="center", 
-    color_text="white" }) {
+    color_text="white",
+    tamano_letra="16px"
+ }) 
+{
 
     const alineacion = {
         "left": "text-start",
@@ -9,9 +12,18 @@ export default function Texto({
         "right": "text-end"
     }[alineado];
 
+    const fontSize = {
+        "1": "fs-1",
+        "2": "fs-2",
+        "3": "fs-3",
+        "4": "fs-4",
+        "5": "fs-5",
+        "6": "fs-6"
+    }[tamano_letra];
+
     return (
 
-        <p className={`${alineacion}`} style={{color: color_text}}>
+        <p className={`${alineacion} ${fontSize}`} style={{color: color_text}}>
             {texto}
         </p>
     )
