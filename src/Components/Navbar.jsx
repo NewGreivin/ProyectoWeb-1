631424cc4cd0f42d36995f7d6381d406a764ed00
Ricardo <richavesca@est.utn.ac.cr>
2026-04-24 18:04:01 -0600

    Se creo el componente Navbar
    Se creo el componente navbar donde se controla como se expande mediante el clases, seguidamente de texto para colocar el titulo del navbar, se usa un navlist en true o false para que se ordene segun el margen, tenemos un links[] encargado de guardar el array de links para luego recorrerlo y mostrarlo y por ultimo los content para poder agregar mas de un componente en su lugar el app.jsx o en la page necesaria.

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,60 @@
+export default function Navbar( { 
+    
+    clases = "navbar-expand-lg", 
+    texto = "Navbar",
+    navList = false,
+    links = [],
+    text_Search = "Search",
+
+    brandContent,
+    buttonContent
+} 
+){
+    
+    //Controla como se expande o se comprime en forma hamburguesa.
+    const navbar_expand = {
+        "sm": "navbar-expand-lg",
+        "md": "navbar-expand-md",
+        "lg": "navbar-expand-lg",
+        "xl": "navbar-expand-xl",
+        "xxl": "navbar-expand-xxl"
+    }[clases];
+
+    const navListClass = navList 
+        ? "me-auto mb-2 mb-lg-0"
+        : "";
+
+    return(
+        <nav className={`navbar ${navbar_expand} bg-body-tertiary`}>
+            <div className="container-fluid">
+                
+                {texto && <a className="navbar-brand" href="#">
+                    {brandContent}
+                    {texto}
+                </a>}
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    {links && <ul className={`navbar-nav ${navListClass}`}>
+                        {links.map((link, index ) => (
+                            <li className="nav-item" key={index}>
+                                <a className={`nav-link ${link.active ? 'active' : ''}`} href={link.url}>
+                                    {link.texto}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>}
+                    
+                    {buttonContent  && <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                        {buttonContent}
+                    </form>}
+                </div>
+            </div>
+        </nav>
+    )
+}
