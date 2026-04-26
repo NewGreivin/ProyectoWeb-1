c56552e10151bb2969024472187983896dab628d
Ricardo <richavesca@est.utn.ac.cr>
2026-04-26 09:09:41 -0600

    Se modifico el card
    Se quito la opcion de los valores hexadecimales para los colores del card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,8 +11,6 @@ export default function Card({
 }
 ) {
 
-    const isHexColor = (color) => /^#[0-9A-F]{6}$/i.test(color);
-
     const color_fondo = {
         "blue": "text-bg-primary",
         "grey": "text-bg-secondary",
@@ -41,14 +39,8 @@ export default function Card({
         "right": "text-end"
     }[alineado];
 
-    const dynamicStyle = {
-        width: "18rem",
-        ...(isHexColor(color_background) && { backgroundColor: color_background }),
-        ...(isHexColor(color_texto) && { color: color_texto })
-    };
-
     return (
-        <div className={`card ${!isHexColor(color_background) ? color_fondo : ''} ${!isHexColor(color_texto) ? color_text : ''} ${alineacion} shadow rounded`} style={dynamicStyle}>
+        <div className={`card ${color_fondo} ${color_text} ${alineacion} shadow rounded`} style={{ width: "18rem" }}>
 
             {header && <div className="card-header">{header}</div>}
 
