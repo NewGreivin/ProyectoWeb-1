292ee224b2f424f4383432986005b208b2c1ca0f
New Greivin <grarguedasgu@est.utn.ac.cr>
2026-04-29 12:29:22 -0600

    Actualización archivo App.jsx
    - contiene la incorporación de todas las paginas existentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import PAnswer_Result from './pages/PAnswerResult';
+import PHome from './pages/PHome';
+import PName from './pages/PName';
+import PConfig from './pages/PConfig';
+import PQuestions from './pages/PQuestions';
+import PAnswerResult from './pages/PAnswerResult';
+import PResult from './pages/PResult';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/answer-result" element={<PAnswer_Result />} />
+        <Route path="/" element={<PHome />} />
+        <Route path="/name" element={<PName />} />
+        <Route path="/config" element={<PConfig />} />
+        <Route path="/questions" element={<PQuestions />} />
+        <Route path="/answer-result" element={<PAnswerResult />} />
+        <Route path="/result" element={<PResult />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
