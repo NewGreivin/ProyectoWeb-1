785f6c61da75b4f5662ddd76a2befb24a467b0b1
Ricardo <richavesca@est.utn.ac.cr>
2026-04-28 22:43:38 -0600

    Se modifico el Select
    Se modifico el Select devido que al extraer las categorias y dificultades del archivo .js no lo permite por los diferentes parametros que tenia, se deja esta version porque especialmente adaptada para esta funcionalidad

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -1,41 +1,22 @@
-export default function Select( { 
-    select = "selectionDefault", 
-    tamano = 1,     
-    class_select = "select_lg", 
-    disable = false, 
-    multiple = false,
+export default function Select({
+    options = [],
+    value,
+    onChange,
+    texto = "Selecciona una opción"
+}) {
+    return (
+        <select
+            className="form-select mt-3"
+            value={value}
+            onChange={onChange}
+        >
+            <option value="">{texto}</option>
 
-    width = "", 
-
-    texto,
-    options = []
-} 
-){
-    
-    const selection = {
-        "selectionDefault": "default select example",
-        "selectionLarge": "large select example",
-        "selectionSmall": "small select example",
-        "selectionMultiple": "multiple select example"
-    }[select];
-    
-    const clase_select = {
-        "select_lg": "form-select-lg",
-        "select_sm": "form-select-sm",
-    }[class_select];
-
-    return(
-        <select className={`form-select ${clase_select}`} size={tamano} aria-label={selection}  disabled={disable} multiple={multiple} style={{width: width}}>
-            {texto && <option value="" disabled>{texto}</option>}
-            {options.map((grupo, index) => (
-                <optgroup key={index} label={grupo.label}>
-                    {grupo.options.map((op, i) => (
-                        <option key={i} value={op.value}>
-                            {op.text}
-                        </option>
-                    ))}
-                </optgroup>
+            {options.map((option, index) => (
+                <option key={index} value={option.value}>
+                    {option.text}
+                </option>
             ))}
         </select>
-    )
+    );
 }
